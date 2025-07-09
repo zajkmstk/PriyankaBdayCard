7e52a5095468f3c0771f83e875fe44b699dbe5a6
Lukasz Zajkowski <lukas1504284@mastek.com>
2025-07-09 16:07:20 +0100

    changed to H2, cleaed up the test code

--- PriyankaBdayCard/src/main/java/com/example/PriyankaBdayCard/PriyankaBdayCardApplication.java
+++ PriyankaBdayCard/src/main/java/com/example/PriyankaBdayCard/PriyankaBdayCardApplication.java
@@ -1,14 +1,12 @@
 package com.example.PriyankaBdayCard;
 
 import com.example.PriyankaBdayCard.entities.Wish;
-import com.example.PriyankaBdayCard.helper.ListFiles;
 import com.example.PriyankaBdayCard.service.WishService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,26 +15,7 @@ public class PriyankaBdayCardApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(PriyankaBdayCardApplication.class, args);
 		WishService service = (WishService) context.getBean("wishService");
-//		Wish newWish = new Wish();
-//		Wish newWish2 = new Wish();
-//		newWish.setName("Zajk");
-//		newWish.setMessage("Have a best day!");
-//		newWish2.setName("Josh");
-//		newWish2.setMessage("Happy Birthday!!!!");
-//
-//		service.saveWish(newWish);
-//		service.saveWish(newWish2);
-//		ListFiles.listFiles();
 
-//
-//		try {
-//			Wish newWish3 = service.jsonToWish();
-//			service.saveWish(newWish3);
-//			System.out.println("json added");
-//
-//		} catch (IOException e) {
-//			System.out.println("exception");
-//		}
 
 		try {
 			List<Wish> listOfWishes = service.jsonToWishes();
