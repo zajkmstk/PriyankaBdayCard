61513ba707ff7ca5271162e68fcc06a81abe1ee2
Lukasz Zajkowski <lukas1504284@mastek.com>
2025-07-09 11:10:44 +0100

    Initial commit

---
+++ PriyankaBdayCard/src/main/java/com/example/PriyankaBdayCard/PriyankaBdayCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.PriyankaBdayCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriyankaBdayCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriyankaBdayCardApplication.class, args);
+	}
+
+}
---
+++ PriyankaBdayCard/src/test/java/com/example/PriyankaBdayCard/PriyankaBdayCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PriyankaBdayCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriyankaBdayCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
